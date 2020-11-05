aaa11c6bfd2d3f14b881c34135b640893c4347d0
Mikael Bärlund <midas@iki.fi>
2020-11-05 22:35:03 +0200

    unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 
 const Header = ({name}) =><h1>{name}</h1>
 
-const Statistic = ({name, count}) => <p>{name} {count}</p>
+const Statistic = ({name, count, units}) => <p>{name} {count} {units}</p>
 const Button = (props) => (
   <button onClick={props.handleClick}>
     {props.text}
@@ -12,12 +12,18 @@ const Button = (props) => (
 )
 
 const Statistics = ({good, neutral, bad}) => {
+  let all = good+neutral+bad
+  let average = (good-bad)/(all===0?1:all)
+  let positive = (good)/(all===0?1:all)
   return (
     <>
       <Header name="statistics"/>
-      <Statistic name="good" count={good}/>
-      <Statistic name="neutral" count={neutral}/>
-      <Statistic name="bad" count={bad}/>
+      <Statistic name="good"     count={good}/>
+      <Statistic name="neutral"  count={neutral}/>
+      <Statistic name="bad"      count={bad}/>
+      <Statistic name="all"      count={all}/>
+      <Statistic name="average"  count={average}  units="%"/>
+      <Statistic name="positive" count={positive} units="%"/>
     </>
   )
 }
@@ -26,9 +32,7 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   
-  const setToValue = newValue => {
-    setGood(newValue)
-  }
+
   return (
     <>
       <Header name="give feedback"/>
