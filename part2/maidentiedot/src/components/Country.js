6d8ddcd98684b449bc1845f71e0fae244307e8bb
Mikael Bärlund <midas@iki.fi>
2020-11-07 00:46:58 +0200

    step3

--- part2/maidentiedot/src/components/Country.js
+++ part2/maidentiedot/src/components/Country.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Country = ({ country }) => {
+    const api_key = process.env.REACT_APP_API_KEY
+    const [weather, setWeather] = useState({ current: { weather_icons: [], weather_descriptions: [] } })
+    const url = `http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`
+    console.log(url)
+    useEffect(() => {
+        axios
+            .get(url)
+            .then(response => {
+                setWeather(response.data)
+            })
+    }, [])
     return (
         <>
             <h2>{country.name}</h2>
@@ -10,7 +22,11 @@ const Country = ({ country }) => {
             <ul>
                 {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
             </ul>
-            <img src={country.flag} width="400px" />
+            <img alt="country flag" src={country.flag} width="250px" />
+            <h3>Weather in {country.capital}</h3>
+            <div><b>temperature:</b> {weather.current.temperature} Celsius</div>
+            <img alt={weather.current.weather_descriptions[0]} src={weather.current.weather_icons[0]} width="100px" />
+            <div><b>wind:</b> {weather.current.wind_speed} mph direction {weather.current.wind_dir} </div>
         </>
     )
 }
